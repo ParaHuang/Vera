5795154e452464e34d6eedc5253149a826352166
ParaHuang <paradox041@gmail.com>
2024-12-28 15:10:48 +0800

    java-day1

---
+++ VeraDemo/src/Main1.java
@@ -0,0 +1,54 @@
+
+public class Main1 {
+	//args:arguments
+	public static void main(String[] args) {//the entrance of your program
+		//code;
+		
+		//print something
+		//String
+		System.out.println("hello");
+		System.out.println("world");
+		
+		//define a variable
+		//type is important
+		
+		//math: a number plus 5 equals 8, what this number is?
+		/*
+		  math:
+		 	x + 5 = 8
+		 */
+		//math: every variable is a number
+		//java: variable could be number(integer, decimal), content(string,character), true/false
+		
+		//primitive type:
+		//short int long          float double          boolean           char           byte
+		//integer number          decimal number       true/false         character
+		
+		//type variableName = value; 
+		int x = 10;	//define a integer value, save the value 10
+		//a double variable, call it pi, save value 3.14
+		double pi = 3.1415926;
+		//define a int variable, call it age , save value 18
+		int age = 8;
+		
+		System.out.println("age");	//print the content age
+		System.out.println(age);	//print the value
+		
+		//1.print some certain content
+		//System.out.println("content");
+		
+		//2.print the value of a variable
+		//System.out.println(variableName);
+		
+		//3.print mixed up stuff(content , variable)
+		//+ : contact
+		//System.out.println(variableName1 + "content1" + variableName2 + "content2");
+		// the age of this cat is 3
+		System.out.println("the age of this cat is " + age);
+		
+		//the pi of a circle is 3.1415
+		System.out.println("the pi of a circle is " + pi);
+		
+		
+	}
+}
