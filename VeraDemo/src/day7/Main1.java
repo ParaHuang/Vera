16fedb36d5f82f1ffe80e645d6bb1617d4e656a9
ParaHuang <paradox041@gmail.com>
2025-04-05 09:55:28 +0800

    for loop

---
+++ VeraDemo/src/day7/Main1.java
@@ -0,0 +1,44 @@
+package day7;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//print hello for 5 times (while loop)
+		
+//		while(something here only correct for 5 times) {
+//			System.out.println("hello");
+//		}
+		/*
+		int i=1;	//loop variable
+		while(i<=5) {
+			System.out.println("hello");			//print:		h1	h2	h3	h4	h5
+			i++;	//i+=1;		//i=i+1;			//i become:		2	3	4	5	6
+		}
+		*/
+		/*
+		//print the number 1~100		3 essential part for a limit times of loop
+		int i=1;							//a.initial value
+		while(i<=100) {						//b.condition
+			System.out.println(i);
+			i++;							//c.increment
+		}
+		
+		System.out.println("i becomes "+i+" in the end");
+		*/
+		
+		/*
+		for(a;b ; c) {
+			
+		}
+		*/
+		
+		for( int i=1  ; i<=100 ;  i++ ) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+
+}
